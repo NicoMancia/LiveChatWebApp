a6ba0c6dcb82ef4de93a4788d28c3093114f9641
nicola <nicolamancia01@gmail.com>
2024-03-23 17:20:20 +0100

    fixed bugs 3.0

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messagesRoutes);
 app.use("/api/users",userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontEnd/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname,"frontEnd","dist","index.html"));
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 })
 
 
